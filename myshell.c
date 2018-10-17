fd201828875ce60ee965a387d7ef6f293271d97f
Likhon Gomes <likhon.dg@gmail.cm>
2018-10-16 23:41:47 -0400

    execvp

--- myshell.c
+++ myshell.c
@@ -53,7 +53,7 @@ void cd(){
 }
 
 void clr(){
-  system("clear");
+  printf("\033[H\033[2J]");
 }
 
 void dir(){
@@ -64,8 +64,14 @@ void environ(){
 
 }
 
-void echo(){
-
+void echo(char** args){
+  int i = 1;
+  while(args[i] != NULL){
+    printf("%s ",args[i]);
+    args[i] = NULL;
+    i++;
+  }
+  printf("\n");
 }
 
 void help(){
@@ -75,22 +81,51 @@ void help(){
 
 void execute(char **args){
 
+  int runBG = 0; // running in the background
+
   if(args[0] == NULL){
     //checking if the args list is empty
   }else{
+
     if(strcmp(args[0],"exit")==0){
       quit();
     } else if (strcmp(args[0],"clear")==0){
       clr();
+    } else if (strcmp(args[0],"echo")==0){
+      echo(args);
+    }else if (strcmp(args[0],"cd")==0){
+      cd();
+    } else if (strcmp(args[0],"dir")==0){
+      dir();
+    } else if (strcmp(args[0],"environ")==0){
+      environ();
+    } else if (strcmp(args[0],"help")==0){
+      help();
+    } else {
+      //not a builtin function
+      pid_t pid = fork();
+      if(pid < 0){
+        printf("fork failed");
+      } else if ( pid == 0){
+        execvp(args[0], args);
+      } else {
+        if(runBG == 0){
+          waitpid(pid, NULL,0);
+        }
+      }
+
+
+    }
+
+
     }
   }
 
   
-}
 
 //This is the main function
 int main(int argc, char *argv[]){
-  system("clear");
+  printf("\033[H\033[2J]");
 
   while (loop != -1) {
     char cwd[1024];
